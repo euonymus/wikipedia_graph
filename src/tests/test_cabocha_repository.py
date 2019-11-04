6f8c3e2a5e7c75be5b278eadf2290babcdcb59db
euonymus <euonymus0220@gmail.com>
2019-11-05 02:35:20 +0900

    chore

--- src/tests/test_cabocha_repository.py
+++ src/tests/test_cabocha_repository.py
@@ -4,7 +4,7 @@ from repositories.cabocha_repository import CabochaRepository
 class TestCabochaRepository(object):
 
     @pytest.mark.parametrize("sentence, expected", [
-        ("浅田真央は伊藤の衣装を着用して競技に臨んだこともあり、「みどりさんの衣装を着るといつも調子がいい」と語っていた", True),
+        ("浅田真央は伊藤緑の衣装を着用して競技に臨んだこともあり、「みどりさんの衣装を着るといつも調子がいい」と語っていた", True),
     ])
     def test_parse(self, sentence, expected):
         # cabocha = CabochaRepository()
