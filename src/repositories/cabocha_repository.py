36705cc2f04c9182053cc0ba52734c31cad8ccc9
euonymus <euonymus0220@gmail.com>
2019-11-05 15:39:17 +0900

    chore

--- src/repositories/cabocha_repository.py
+++ src/repositories/cabocha_repository.py
@@ -2,11 +2,13 @@ class CabochaRepository(object):
 
     def __init__(self):
         import CaboCha
-        self.cabocha = CaboCha.Parser()
-
+        self.cabocha = CaboCha.Parser("-d /usr/local/lib/mecab/dic/mecab-ipadic-neologd")
         
     def parse(self, sentence):
         import CaboCha
         tree = self.cabocha.parse(sentence)
-        return tree.toString(CaboCha.CABOCHA_FORMAT_TREE)
-
+        tree_format =  tree.toString(CaboCha.FORMAT_TREE)
+        lattice_format = tree.toString(CaboCha.FORMAT_LATTICE)
+        print(tree_format)
+        print(lattice_format)
+        return tree_format
