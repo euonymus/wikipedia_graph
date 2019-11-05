6df25124e0d74fa0add7d3bba3178a43f8ad505c
euonymus <euonymus0220@gmail.com>
2019-11-05 17:56:13 +0900

    try

--- src/repositories/cabocha_repository.py
+++ src/repositories/cabocha_repository.py
@@ -7,6 +7,20 @@ class CabochaRepository(object):
     def parse(self, sentence):
         import CaboCha
         tree = self.cabocha.parse(sentence)
+
+        chunkId = 0
+        for i in range(0, tree.size()):
+            token = tree.token(i)
+            if token.chunk != None:
+                print(chunkId, token.chunk.link, token.chunk.head_pos, 
+                      token.chunk.func_pos, token.chunk.score)
+                chunkId += 1
+
+            print('========================')
+            print(token.surface, token.feature, token.ne)
+            print('-----------------------')
+
+
         tree_format =  tree.toString(CaboCha.FORMAT_TREE)
         lattice_format = tree.toString(CaboCha.FORMAT_LATTICE)
         print(tree_format)
